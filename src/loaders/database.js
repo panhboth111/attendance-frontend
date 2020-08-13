c94b4447f7846dcd9da2b198609bda640cf3bf2e
Hourn Sovannara <hournsovannara18@kit.edu.kh>
2020-08-13 22:44:21 +0700

    Change DB interface

--- src/loaders/database.js
+++ src/loaders/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   const connection = await mongoose.connect(
-    "mongodb://localhost:27017/attendance-app",
+    "mongodb://0.0.0.0:27017/attendance-app",
     { useNewUrlParser: true, useUnifiedTopology: true }
   );
   return connection.connection.db;
